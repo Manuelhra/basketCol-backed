678ef32b10b2ad83002ba51388ba6a721cb5aa59
manuelh_ra <manuelhra.dev@gmail.com>
2024-09-03 11:41:04 -0500

    Update package domain basketcol

--- src/basketCol/users/player/application/use-cases/CreatePlayerUserUseCase.ts
+++ src/basketCol/users/player/application/use-cases/CreatePlayerUserUseCase.ts
@@ -46,6 +46,7 @@ export class CreatePlayerUserUseCase {
       id,
       name,
       biography,
+      nickname,
       email,
       password,
     } = payload;
@@ -65,6 +66,7 @@ export class CreatePlayerUserUseCase {
       playerUserId.value,
       name,
       biography,
+      nickname,
       playerUserEmail.value,
       playerUserPassword.value,
       active,
